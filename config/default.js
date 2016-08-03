65f71e57c73217db8453d8247866bfa9996238ce
James L. Chamberlain <jchamber@us.ibm.com>
2016-08-03 10:34:54 -0400

    Testing2

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 //
 var config = {
-    response : "Yeah! You have reached pages.github.ibm.com.",
+    response : "Yeah!! You have reached pages.github.ibm.com.",
     port : process.env.PORT || process.env.VCAP_APP_PORT || 443
 };
 
