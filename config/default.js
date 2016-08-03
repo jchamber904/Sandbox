3d3fc42ab9caab220a1c10e5a9a3d12b36bec739
James L. Chamberlain <jchamber@us.ibm.com>
2016-08-03 10:24:54 -0400

    Testing

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 var config = {
-    response : "Hello, world! This is a demo app.",
+    response : "Hello, world! This is a demo app..",
     port : process.env.PORT || process.env.VCAP_APP_PORT || 3000
 };
 
